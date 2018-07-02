f3ad77459f3c27d3e06dde14de116ecc4decdd3d
Ilesha Sawarkar <ilesha01@gmail,com>
2018-07-02 15:27:10 +0530

    app_icon

--- IndianLaws/app/build/generated/source/r/debug/com/the3hdmedia/IndianLaws/R.java
+++ IndianLaws/app/build/generated/source/r/debug/com/the3hdmedia/IndianLaws/R.java
@@ -3923,6 +3923,7 @@ public final class R {
   public static final class mipmap {
     public static final int ic_launcher=0x7f0c0000;
     public static final int ic_launcher_round=0x7f0c0001;
+    public static final int justice_two_hands=0x7f0c0002;
   }
   public static final class string {
     public static final int abc_action_bar_home_description=0x7f0d0000;
